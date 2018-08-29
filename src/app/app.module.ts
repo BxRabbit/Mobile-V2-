6a6d890d5f180eac23c7607a9a74c544f75ae2d7
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-08-28 21:35:02 -0500

    Version 1.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage, CalculatorPage, LoginPage, TabsPage, EditPage, LibraryPage, PrincipalPage, HttpModule,
+  DoPoollPage, SeePollPage, ResultpollsPage, ConsultaProvider } from '../pages/index.paginas';
 
-import { LoginPage } from '../pages/login/login';
-import { CalculatorPage } from '../pages/calculator/calculator';
-import { CreatePage } from '../pages/create/create';
-import { EditPage } from '../pages/edit/edit';
-import { LibraryPage } from '../pages/library/library';
-import { SignupPage } from '../pages/signup/signup';
-import { PrincipalPage } from '../pages/principal/principal';
-import { CreateAskPage } from '../pages/create-ask/create-ask';
-import { HttpModule } from '@angular/http';
-import { DoPoollPage } from '../pages/do-pooll/do-pooll';
-import { GraphicPage } from '../pages/graphic/graphic';
-import { SeePollPage } from '../pages/see-poll/see-poll';
-import { ResultpollsPage } from '../pages/resultpolls/resultpolls';
-import { ConsultaProvider } from '../providers/consulta/consulta';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -30,22 +14,16 @@ import { AuthSevice } from '../services/auth/auth';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     LoginPage,
     CalculatorPage,
-    CreatePage,
     DoPoollPage,
-    GraphicPage,
     ResultpollsPage,
     SeePollPage,
     PrincipalPage,
     EditPage,
-    SignupPage,
     LibraryPage,
     TabsPage,
-    CreateAskPage
   ],
   imports: [
     BrowserModule,
@@ -70,20 +48,14 @@ import { AuthSevice } from '../services/auth/auth';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     LoginPage,
     DoPoollPage,
-    GraphicPage,
     SeePollPage,
     CalculatorPage,
     ResultpollsPage,
-    CreatePage,
     EditPage,
-    CreateAskPage,
     PrincipalPage,
-    SignupPage,
     LibraryPage,
     TabsPage
   ],
--- src/pages/index.paginas.ts
+++ src/pages/index.paginas.ts
@@ -2,7 +2,7 @@ export { Component, NgModule } from '@angular/core';
 
 export { FormBuilder } from '@angular/forms';
 
-export { Http, Response } from '@angular/http';
+export { Http, HttpModule, Response } from '@angular/http';
 
 export { IonicPage, NavController, NavParams, ToastController, IonicPageModule, AlertController,
     LoadingController } from 'ionic-angular';
@@ -10,18 +10,15 @@ export { IonicPage, NavController, NavParams, ToastController, IonicPageModule, 
 export { AuthSevice } from '../services/auth/auth';
 export { ConsultaProvider} from '../providers/consulta/consulta';
 
-export { CreateAskPage } from '../pages/create-ask/create-ask';
 export { LoginPage } from '../pages/login/login';
-export { SignupPage } from '../pages/signup/signup';
 export { PrincipalPage } from '../pages/principal/principal';
-export { CreatePage } from '../pages/create/create';
 export { ResultpollsPage } from '../pages/resultpolls/resultpolls';
 export { DoPoollPage } from '../pages/do-pooll/do-pooll';
 export { SeePollPage} from '../pages/see-poll/see-poll';
-export { GraphicPage } from '../pages/graphic/graphic';
 export { HomePage } from '../pages/home/home';
 export { CalculatorPage } from '../pages/calculator/calculator';
 export { EditPage } from '../pages/edit/edit';
 export { LibraryPage } from '../pages/library/library';
+export { TabsPage } from '../pages/tabs/tabs';
 
 
